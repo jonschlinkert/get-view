44451344b97e6d97c7cd2896e9b7e9393ac369c4
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-25 00:13:13 -0400

    ensure file is an object, and has a path property

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 
 'use strict';
 
+var isObject = require('isobject');
 var isMatch = require('match-file');
 var path = require('path');
 
@@ -21,6 +22,12 @@ module.exports = function getFile(name, collection, fn) {
 
   for (var key in collection) {
     file = collection[key];
+    if (!isObject(file)) {
+      throw new TypeError('expected file to be an object');
+    }
+    if (typeof file.path !== 'string') {
+      throw new TypeError('expected file.path to be a string');
+    }
     if (isMatch(name, file)) {
       return file;
     }
