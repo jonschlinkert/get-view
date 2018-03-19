df4532a091bba82ae8ef151beff7a86816d0c9ef
jonschlinkert <jon.schlinkert@sellside.com>
2018-03-18 20:22:04 -0400

    refactor

--- index.js
+++ index.js
@@ -7,30 +7,56 @@
 
 'use strict';
 
-var isMatch = require('match-file');
-var path = require('path');
+const path = require('path');
+const endsWith = require('path-ends-with');
 
-module.exports = function getFile(name, collection, fn) {
-  var file = collection[name];
-  if (file) return file;
-
-  if (typeof fn === 'function') {
-    file = collection[fn(name)];
-    if (file) return file;
+module.exports = function(collection, name) {
+  if (!collection || typeof collection !== 'object') {
+    throw new TypeError('expected the first argument to be an object');
   }
 
-  for (var key in collection) {
-    file = collection[key];
-    if (!isVinyl(file)) {
-      throw new TypeError('expected file to be an object');
+  if (collection[name]) return collection[name];
+
+  for (const key of Object.keys(collection)) {
+    const view = collection[key];
+    if (name === view.path) return view;
+    if (name === relative(view)) return view;
+    if (name === basename(view)) return view;
+    if (name === stem(view)) return view;
+
+    if (typeof name === 'function') {
+      if (name(view) === true) {
+        return view;
+      }
+      continue;
     }
-    if (typeof file.path !== 'string') return;
-    if (isMatch(name, file)) {
-      return file;
+
+    if (typeof view.hasPath === 'function' && view.hasPath(name)) {
+      return view;
+    }
+
+    if (name instanceof RegExp) {
+      if (name.test(view.path) || (view.history && name.test(view.history[0]))) {
+        return view;
+      }
+      continue;
+    }
+
+    if (endsWith(view.path, name) || (view.history && endsWith(view.history[0], name))) {
+      return view;
     }
   }
 };
 
-function isVinyl(file) {
-  return file && typeof file === 'object' && file._isVinyl === true;
+function stem(view) {
+  return view.stem || path.basename(view.path, path.extname(view.path));
+}
+
+function basename(view) {
+  return view.basename || path.basename(view.path);
+}
+
+function relative(view) {
+  const cwd = view.cwd || process.cwd();
+  return view.relative || path.relative(cwd, view.path);
 }
--- test.js
+++ test.js
@@ -1,62 +1,59 @@
 'use strict';
 
 require('mocha');
-var path = require('path');
-var assert = require('assert');
-var Templates = require('templates');
-var getView = require('./');
-var app;
+const assert = require('assert');
+const getView = require('./');
+let views;
 
 describe('getView', function() {
   beforeEach(function() {
-    app = new Templates();
-    app.create('page');
-
-    app.page('foo', { content: 'this is foo' });
-    app.page('bar.md', { content: 'this is bar' });
-    app.page('a/b/c/baz.md', { content: 'this is baz', base: 'a' });
-    app.page('test/fixtures/templates/a.tmpl');
+    views = {
+      'foo': { path: 'foo', content: 'this is foo' },
+      'bar.md': { path: 'bar.md', content: 'this is bar' },
+      'a/b/c/baz.md': { path: 'a/b/c/baz.md', content: 'this is baz', base: 'a' },
+      'test/fixtures/templates/a.tmpl': { path: 'test/fixtures/templates/a.tmpl' }
+    };
   });
 
   it('should get a view by name', function() {
-    var view = getView('foo', app.views.pages);
+    const view = getView(views, 'foo');
     assert(view);
-    assert.equal(view.key, 'foo');
+    assert.equal(view.path, 'foo');
   });
 
   it('should return undefined when a view is not found', function() {
-    var view = getView('flflflflflf', app.views.pages);
+    const view = getView(views, 'flflflflflf');
     assert(typeof view === 'undefined');
   });
 
   it('should get a view with the key modified by the given function', function() {
-    var view = getView('foo.md', app.views.pages, function(key) {
-      return path.basename(key, path.extname(key));
+    const view = getView(views, function(view) {
+      return view.path.toUpperCase() === 'FOO';
     });
     assert(view);
-    assert.equal(view.key, 'foo');
+    assert.equal(view.path, 'foo');
   });
 
   it('should get a view by `view.path`', function() {
-    var view = getView('a/b/c/baz.md', app.views.pages);
+    const view = getView(views, 'a/b/c/baz.md');
     assert(view);
     assert.equal(view.path, 'a/b/c/baz.md');
   });
 
   it('should get a view by `view.basename`', function() {
-    var view = getView('baz.md', app.views.pages);
+    const view = getView(views, 'baz.md');
     assert(view);
     assert.equal(view.path, 'a/b/c/baz.md');
   });
 
-  it('should get a view by `view.filename`', function() {
-    var view = getView('baz', app.views.pages);
+  it('should get a view by `view.stem`', function() {
+    const view = getView(views, 'baz');
     assert(view);
     assert.equal(view.path, 'a/b/c/baz.md');
   });
 
   it('should get a view by `view.relative`', function() {
-    var view = getView('b/c/baz.md', app.views.pages);
+    const view = getView(views, 'b/c/baz.md');
     assert(view);
     assert.equal(view.path, 'a/b/c/baz.md');
   });
